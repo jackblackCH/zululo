4c56324f8e9ac2e6d0b690e3e50d24c47f2bc671
agatta1504 <agatta1504@gmail.com>
2017-03-01 16:14:26 +0000

    Fix import

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../style/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        hello
       </div>
     );
   }
