4e330251073f48ca7d4167b3a3b226e6dfe0f6e5
agatta1504 <agatta1504@gmail.com>
2017-03-02 15:13:42 +0000

    Add image

--- src/components/ShowCountries.js
+++ src/components/ShowCountries.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../style/App.css';
 import {countryList} from '../actions/index'
+import {Link} from 'react-router'
+
 
 class ShowCountries extends Component {
 
@@ -11,7 +13,10 @@ class ShowCountries extends Component {
         <ul>
         {this.props.countries.map(country=> {
           return (
-              <li>{country.name}</li>
+            <li>
+              <Link to='/'>{country.name}</Link>
+              <img src={country.flag} />
+            </li>
           )
         })}
       </ul>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -21,7 +21,7 @@ const INIT_STATE = {
 }
 const reducer = (state = INIT_STATE, action) => {
 	switch (action.type) {
-		
+
 		default:
 			return state;
 	}
