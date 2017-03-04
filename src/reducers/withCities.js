a533653aae8c72db5812e6e5418ce8d44d7d4b47
Marc Illien <marc.illien@siroop.ch>
2017-03-04 11:51:32 +0000

    Add images

--- src/reducers/withCities.js
+++ src/reducers/withCities.js
@@ -1,9 +1,26 @@
 import cities from '../models/cities';
+import Flickr from 'node-flickr'
+const keys = {"api_key": "3ad6a7e064627c39b93ecaaed3fb40e4"}
+const flickr = new Flickr(keys);
+
+// function getFlickrPhoto(tag, cb) {
+//   flickr.get("photos.search", {"tags": tag }, function(err, result){
+//       if (err) return;
+//       flickr.get("photos.getSizes", {"photo_id": result.photos.photo[0].id }, function(err, result) {
+//         if (err) return console.error(err);
+//           if (result.sizes.size[6]) {
+//             console.log(result.sizes.size[6].source);
+//             return result.sizes.size[6].source;
+//           }
+//         });
+//   });
+// }
 
 const filteredCities = cities.map( city => {
   return {
     ...city,
-    id: city.name
+    id: city.name,
+    image: city.photo
   }
 })
 
